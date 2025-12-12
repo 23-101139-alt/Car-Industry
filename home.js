9df75957d9735c77b4eb2b5571845bb5a02c1e31
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-12 20:07:45 +0200

    header-structure

---
+++ home.js
@@ -0,0 +1,46 @@
+// header
+document.getElementById("list1").innerHTML = "About Us";
+document.getElementById("list2").innerHTML = "Suppliers";
+document.getElementById("list3").innerHTML = "Products";
+document.getElementById("list4").innerHTML = "Contact Us";
+// header
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
