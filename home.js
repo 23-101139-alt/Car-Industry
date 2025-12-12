6d837275a29416a9e49dd4580a0e484d5484d78c
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-12 20:51:38 +0200

    burger-menu-responsive

--- home.js
+++ home.js
@@ -3,6 +3,20 @@ document.getElementById("list1").innerHTML = "About Us";
 document.getElementById("list2").innerHTML = "Suppliers";
 document.getElementById("list3").innerHTML = "Products";
 document.getElementById("list4").innerHTML = "Contact Us";
+
+
+document.getElementById("list1bg").innerHTML = "About Us";
+document.getElementById("list2bg").innerHTML = "Suppliers";
+document.getElementById("list3bg").innerHTML = "Products";
+document.getElementById("list4bg").innerHTML = "Contact Us";
+
+
+  document.querySelector('.burger-menu-cont').addEventListener('click', () => {
+    document.querySelector('.dropdown-burger-menu').classList.toggle('show');
+  });
+
+
+
 // header
 
 
