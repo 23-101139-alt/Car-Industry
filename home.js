55a5c76db153070f0f9a63de9a2523c74b0b51e4
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-13 11:53:55 +0200

    3d-models-sec2

--- home.js
+++ home.js
@@ -99,9 +99,71 @@ gsap.from(".cont-left-sec1", {
 
 
 
+// sec2
+document.getElementById("header-sec2").innerHTML = "Featured Models";
 
 
+let cardssec2 = [
+  { model: "models/2024_byd_yangwang_u9.glb",
+     poster: `models/poster2.png`,
+    title:`BYD Yangwang U9`,
+    stitle:`An electric supercar built for extreme performance.`,
+ height: "516px" },
+      { model: "models/2024_byd_seal.glb",
+     poster: `models/poster3.png`,
+    title:`BYD Seal`,
+    stitle:`A refined EV crafted for comfort and agility.` ,
+ height: "264px" },
+       { model: "models/2019_audi_r8_coupe.glb",
+     poster: `models/poster4.png`,
+    title:`Audi_r8_coupe`,
+    stitle:`Advanced electric performance with premium style.` ,
+ height: "516px" },
+        { model: "models/2020_bmw_m850i_gran_coupe.glb",
+     poster: `models/poster5.png`,
+    title:`BMW M850i Gran Coupe`,
+    stitle:`A perfect blend of speed, comfort, and precision.` ,
+ height: "516px" },
 
+];
+
+for (let i = 0; i < cardssec2.length; i++) {
+
+  document.getElementById("cards-sec2").innerHTML += `
+                    <div class="main-card-sec2">
+                            <div class="model-container">
+                            <model-viewer 
+                                src="${cardssec2[i].model}"
+                                ar ar-modes="webxr scene-viewer quick-look"
+                                camera-controls 
+                                tone-mapping="neutral" 
+                                poster="${cardssec2[i].poster}" 
+                                class="models-sec2"
+                                camera-orbit="-90deg 90deg 1.2m"
+                                min-camera-orbit="0.5m"
+                                max-camera-orbit="3m"
+                                style="height: ${cardssec2[i].height}; width: 100%;"
+                                shadow-intensity="1"
+ 
+                                camera-target="0 0 0"
+                                disable-zoom
+                                disable-tap>
+                            </model-viewer>
+                                <button class="cta-sec2">
+                                    <a href="#" class="link-sec2">
+                                        <img src="pic/pic9.png">
+                                    </a>
+                                </button>
+
+                            </div>
+                            <div class="text-box-sec2">
+                                <h2 class="title-sec2">${cardssec2[i].title}</h2>
+                                <h3 class="small-text-sec2">${cardssec2[i].stitle}</h3>
+                            </div>
+                        </div>
+
+  `;
+}
 
 
 
