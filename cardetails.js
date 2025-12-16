3b191e4bbfb2a4befc73f94fc665e9cb29b52499
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-16 20:56:24 +0200

    page2-sec1-text-box

---
+++ cardetails.js
@@ -0,0 +1,59 @@
+// header
+document.getElementById("list1").innerHTML = "About Us";
+document.getElementById("list2").innerHTML = "Suppliers";
+document.getElementById("list3").innerHTML = "Products";
+document.getElementById("list4").innerHTML = "Contact Us";
+
+
+document.getElementById("list1bg").innerHTML = "About Us";
+document.getElementById("list2bg").innerHTML = "Suppliers";
+document.getElementById("list3bg").innerHTML = "Products";
+document.getElementById("list4bg").innerHTML = "Contact Us";
+
+
+  document.querySelector('.burger-menu-cont').addEventListener('click', () => {
+    document.querySelector('.dropdown-burger-menu').classList.toggle('show');
+  });
+
+// header
+
+// sec1
+document.getElementById("haeder-sec1-p2").innerHTML = "BYD Seal U DM-i";
+document.getElementById("feature1").innerHTML = "Model:";
+document.getElementById("answer1").innerHTML = "Seal U DM-i";
+document.getElementById("feature2").innerHTML = "Year:";
+document.getElementById("answer2").innerHTML = "2024";
+document.getElementById("feature3").innerHTML = "Category:";
+document.getElementById("answer3").innerHTML = "Plug-in Hybrid SUV";
+document.getElementById("feature4").innerHTML = "Car’s color:";
+
+document.getElementById("price").innerHTML = "2,650,000 EGP ";
+document.getElementById("cta-text").innerHTML = "Book Now";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
